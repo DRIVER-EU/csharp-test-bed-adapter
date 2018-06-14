6564fd73573dc897749f74c1a5a27ce5b66bb41d
Martijn Hendriks <hendriks@xvrsim.com>
2018-06-14 10:50:36 +0200

    updated all avro schemas because of changes to the CAP message.

--- examples/CSharpExampleProducerStandard/Program.cs
+++ examples/CSharpExampleProducerStandard/Program.cs
@@ -93,6 +93,7 @@ namespace CSharpExampleProducerStandard
                                         uri = "null",
                                         deferUri = "null",
                                         digest = "null",
+                                        mimeType = "null",
                                     },
                                 },
                                 area = new Area
