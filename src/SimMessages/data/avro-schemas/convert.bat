a4c3c70613170b63468d8963327ae56675af17b5
Martijn Hendriks <hendriks@xvrsim.com>
2020-01-15 17:34:26 +0100

    Removed the SimMessages project, since this is now a separate Nuget package called CssMessages (CssMessages is being referenced inside this project). Publising a new version of the test-bed adapter onto Nuget.

--- src/SimMessages/data/avro-schemas/convert.bat
+++
@@ -1,9 +0,0 @@
-REM Call the avrogen.exe to convert all schemas [-s] located in this folder [file path] and output in folder [folder path]
-if exist ..\..\schemas\eu (
-rmdir ..\..\schemas\eu /s /q
-)
-
-for %%a in (*.avsc) do (
-.\avrogen.exe -s %%a ..\..\schemas
-)
-@pause
